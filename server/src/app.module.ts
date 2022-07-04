d28525c3682ecbed4174523eb5f0ecf4674419de
Andrew Horn <andyjhorn@gmail.com>
2022-07-03 23:33:10 -0500

    removed app controller and service

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { resolve } from 'path';
 
@@ -10,7 +8,5 @@ import { resolve } from 'path';
       rootPath: resolve(__dirname, 'client')
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule { }
